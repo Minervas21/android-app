69e87fec43239ec16894ea7a697362c1ac794306
Agboola Olamidipupo Favour <olamidipupofavour@codecube.com.ng>
2022-08-16 22:06:47 +0100

    fixing uo index page

--- src/main.py
+++ src/main.py
@@ -8,13 +8,17 @@ from kivy.core.window import Window
 from kivy.clock import Clock, mainthread
 
 from kivy.logger import Logger
-
+import json
+from kivy.network.urlrequest import UrlRequest
 from kivy.base import platform
 from kivy.uix.screenmanager import ScreenManager,SwapTransition
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivymd.uix.screen import MDScreen
 
 from kivy.uix.label import Label
+sm = ScreenManager(transition=SwapTransition())
+token=None
 class LoginScreen(MDScreen):
 	def signin(self):
 		PythonActivity = autoclass('org.kivy.android.PythonActivity')
@@ -26,9 +30,57 @@ class LoginScreen(MDScreen):
 		currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
 		currentActivity.startActivity(intent)
 class IndexScreen(MDScreen):
-	pass
-sm = ScreenManager(transition=SwapTransition())
-token=None
+	gender=None
+	email=None
+	full_name=None
+	is_admin=None
+	matric_no=None
+	mode=None
+	nickname=None
+	personal_email=None
+	phone_number=None
+	profile_pic=None
+	reg_no=None
+	signature=None
+	theme=None
+	verified=None
+	nav_drawer = ObjectProperty()
+	screen_manager = ObjectProperty()
+	def gotten_info(self,req,result):
+		print(result)
+		#resp=json.loads(result.decode())
+		resp=result
+		self.gender=resp['Gender']
+		self.email=resp['email']
+		self.full_name=resp['full_name']
+		self.is_admin=resp['is_admin']
+		self.matric_no=resp['matric_no']
+		self.mode=resp['mode']
+		self.nickname=resp['nickname']
+		self.personal_email=resp['personal_email']
+		self.phone_number=resp['phone_number']
+		self.profile_pic=resp['profile_pic']
+		self.reg_no=resp['reg_no']
+		self.signature=resp['signature']
+		self.theme=resp['theme']
+		self.verified=resp['verified']
+		profile_pic=self.profile_pic
+		self.ids.spinner.active=False
+		self.ids.avatar.source=self.profile_pic
+		self.ids.name.text=self.full_name
+		self.ids.email.text=self.email
+		#toast(resp['Gender'])
+	def on_enter(self):
+		import requests
+		from requests.structures import CaseInsensitiveDict
+		url = "https://minervasapi.herokuapp.com/get_info"
+		headers = CaseInsensitiveDict()
+		headers["Accept"] = "application/json"
+		headers["Authorization"] = f"Bearer {token}"
+		#headers["Content-Type"] = "application/json"
+		data="{}"
+		#resp = requests.get(url, headers=headers)
+		resp=UrlRequest(url,self.gotten_info,req_headers=headers)
 class MinervasApp(MDApp):
 
     processing_uri = False
